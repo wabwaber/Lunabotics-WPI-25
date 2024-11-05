74869c74c2f8afb48162f91f11cab08393ad90db
Matthew Copeland <mjcmjc2001@gmail.com>
2024-11-04 20:12:50 -0500

    work on gazebo sim
    I started work on the gazebo simulation of the robot, I am creating a base model so I can wrap  my head around it first. I also edited a few of the comments in the horror.py file (these comments were the ones sent in the weekly progress report)

--- horror.py
+++ horror.py
@@ -3,12 +3,6 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-import tkinter as tk
-from tkinter import ttk, font
-#simpy to use for the simulation part of the equations
-#W is the only changing var the rest are provided by user
-
-
 """
 Cases go
 1: - - -
@@ -43,10 +37,8 @@ def boo(a, b, c, d, RtoFind):
     repeatedNum =  bCubedabc + (((-4*(((b**2)-3*a*c+12*d)**3)) + ((bCubedabc) ** 2)) ** (1/2)) #This sequence is repeated in the formula so I am doing those calculations here
         
     #cool we should have all that we need to do this
-    #also making the square roots into fractional exponents for ease of change
+    #also making the square roots into fractional exponents for ease of change (especially with the 1/3 exponents)
     return (-a/4) - cofa * (1/2) *  (((a**2)/4 - (2*b)/3 + ((2**(1/3)) * (b**2 - 3*a*c + 12*d))/(3 * (repeatedNum**(1/3))) + (((repeatedNum)/(54))**(1/3)))**(1/2)) - cofb * 1/2 * ((((a**2)/2) - ((4*b)/3) - (((2**(1/3))*((b**2)-3*a*c+12*d))/(3*((repeatedNum)**(1/3)))) - (((repeatedNum)/(54))**(1/3)) - cofa * ((-a**3 + 4*a*b - 8*c) / (4*((( (a**2/4) - ((2*b)/3) + (((2**(1/3)) * ((b**2) - (3*a*c) + (12*d))) / (3*((repeatedNum)**(1/3)))) + (((repeatedNum) / (54))**(1/3))))**(1/2)))))**(1/2)) #the hard part
-
-
 
 nS = 1
 rS = 0.1
@@ -54,11 +46,8 @@ wS = 135*9.81
 cS = 0.37 #not the same for boo function
 bS = 0.05 #not the same for boo function
 kS = 40000000
-
 rCase = 1 #rCase is the case that needs to be found. it should be an int between 1 & 4
 listOfRoots = {}
-
-print(listOfRoots)
 
 def getSlippagePoint(xValue):
     a = -2 * rS
@@ -97,7 +86,6 @@ def resistance(weight):
         temp = boo(a, b, c, d, 3) #this is the scary part
     except:
         print("ERROR: 3 didn't work :(")
-    print(temp)
     return (kS * bS * ((temp**(nS + 1))/(nS + 1)) + muR * (weight/4))
 
 resisX = np.arange(xRLower, xRUpper, 5)
@@ -105,21 +93,23 @@ resisY = resistance(resisX)
 
 #functions to plot with
 
+#graph wheel resistance
 fige, axe = plt.subplots()
 axe.plot(resisX, resisY)
 axe.set(xlabel="Robot Weight (N)", ylabel="Wheel Resistance (N)", title="Robot Wheel Resistance with Respect to Robot Weight")
 axe.grid()
 fige.savefig("resistance.png")
 
+
+#graph wheel sinkage (I start calling it wheel slippage internally at some point in writing this script)
 fig, ax = plt.subplots()
 ax.plot(slipX, slipY)
 ax.set(xlabel="Robot Weight (N)", ylabel="Sinkage (m)", title="Robot Sinkage with Respect to Robot Weight")
 ax.grid()
 fig.savefig("slip.png")
-plt.show()
 
 plt.show()
+plt.show()#here just in case
 
-#sources for script
-
+#sources of information for script
 # https://matplotlib.org/stable/gallery/lines_bars_and_markers/simple_plot.html#sphx-glr-gallery-lines-bars-and-markers-simple-plot-py
