7f61d8d464bb215b68025fb53b335ecffec73882
Sam Rooney <the.sam.rooney@gmail.com>
2024-11-15 00:32:04 -0500

    update launchfile to launch gazebo (no urdf spawn yet)

--- ros2_ws/src/lunabot_demo/launch/demo.launch.py
+++ ros2_ws/src/lunabot_demo/launch/demo.launch.py
@@ -1,22 +1,34 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription, LaunchDescriptionSource
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Shutdown
-from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import SetLaunchConfiguration, Shutdown
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    # pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    # gz_launch_path = os.join(
-        # pkg_ros_gz_sim, 'launch/gz_sim.launch.py')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    # pkg_luna_gz = get_package_share_directory('luna_gz')
+    pkg_luna_urdf = get_package_share_directory('luna_urdf')
+    gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
+    gz_model_path = PathJoinSubstitution([pkg_luna_urdf, 'luna_urdf'])
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    urdf_file_name = 'luna_urdf/robot.urdf'
-    rviz_file_name = 'demo.rviz'
+    urdf_file_name = 'robot.urdf'
     urdf = os.path.join(
         get_package_share_directory('luna_urdf'),
+        'luna_urdf',
         urdf_file_name)
+    # sdf_file_name = 'robot.sdf'
+    # sdf = os.path.join(
+    #     get_package_share_directory('luna_urdf'),
+    #     'luna_sdf',
+    #     sdf_file_name)
+    rviz_file_name = 'demo.rviz'
     rviz = os.path.join(
         get_package_share_directory('lunabot_demo'),
         rviz_file_name)
@@ -27,7 +39,8 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+            description='Use simulation (Gazebo) clock if true'
+        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -45,39 +58,47 @@ def generate_launch_description():
             executable='static_transform_publisher',
             name='robot_odom_tf',
             output='screen',
-            arguments=["0", "0", "0", "0", "0", "0", "/axis", "/robot"]
+            arguments=['0', '0', '0', '0', '0', '0', '/axis', '/robot']
         ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='robot_baselink_tf',
             output='screen',
-            arguments=["0", "0", "0", "0", "0", "0", "/robot", "/base_link"]
+            arguments=['0', '0', '0', '0', '0', '0', '/robot', '/base_link']
         ),
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=["-d", [rviz]],
+            arguments=['-d', [rviz]],
             on_exit=Shutdown()
         ),
-        # IncludeLaunchDescription(
-        #     LaunchDescriptionSource(gz_launch_path),
-        #     launch_arguments={
-        #         'gz_args': [PathJoinSubstitution([pkg_spaceros_gz_sim, 'worlds',
-        #                                           LaunchConfiguration('world_file')])],
-        #         'on_exit_shutdown': 'True'
-        #     }.items(),
-        # ),
-        # Node(
-        #     package='gazebo_ros', 
-        #     executable='spawn_entity.py',
-        #     arguments=['-entity', "mini_urdf", 
-        #         '-topic', 'robot_description',
-        #             '-x', 0,
-        #             '-y', 0,
-        #             '-z', 0,
-        #             '-Y', 0],
-        #             output='screen')
+        DeclareLaunchArgument(
+            'world',
+            default_value='empty',
+            choices=['empty', 'moon', 'mars', 'enceladus'],
+            description='World to load into Gazebo'
+        ),
+        SetLaunchConfiguration(name='world_file',
+                               value=[LaunchConfiguration('world'),
+                                      TextSubstitution(text='.sdf')]),
+        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                # 'gz_args': [PathJoinSubstitution([pkg_luna_gz, 'worlds',
+                #                                   LaunchConfiguration('world_file')])],
+                'gz_args': ['empty.sdf'],
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[],
+            remappings=[],
+            output='screen'
+        ),
     ])
