92fb48c321b70691e9c4951f1e6e62cbf563d2cf
Sam Rooney <the.sam.rooney@gmail.com>
2024-11-14 21:23:55 -0500

    draft including onshape-to-robot files

--- ros2_ws/src/lunabot_demo/launch/demo.launch.py
+++ ros2_ws/src/lunabot_demo/launch/demo.launch.py
@@ -1,18 +1,25 @@
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch import LaunchDescription, LaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    # gz_launch_path = os.join(
+        # pkg_ros_gz_sim, 'launch/gz_sim.launch.py')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     urdf_file_name = 'urdf/mini_urdf.urdf'
+    rviz_file_name = 'demo.rviz'
     urdf = os.path.join(
         get_package_share_directory('mini_urdf'),
         urdf_file_name)
+    rviz = os.path.join(
+        get_package_share_directory('lunabot_demo'),
+        rviz_file_name)
     with open(urdf, 'r') as infp:
         robot_desc = infp.read()
 
@@ -51,6 +58,25 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen'
+            output='screen',
+            arguments=["-d", [rviz]]
         ),
+        # IncludeLaunchDescription(
+        #     LaunchDescriptionSource(gz_launch_path),
+        #     launch_arguments={
+        #         'gz_args': [PathJoinSubstitution([pkg_spaceros_gz_sim, 'worlds',
+        #                                           LaunchConfiguration('world_file')])],
+        #         'on_exit_shutdown': 'True'
+        #     }.items(),
+        # ),
+        # Node(
+        #     package='gazebo_ros', 
+        #     executable='spawn_entity.py',
+        #     arguments=['-entity', "mini_urdf", 
+        #         '-topic', 'robot_description',
+        #             '-x', 0,
+        #             '-y', 0,
+        #             '-z', 0,
+        #             '-Y', 0],
+        #             output='screen')
     ])
--- ros2_ws/src/lunabot_demo/setup.py
+++ ros2_ws/src/lunabot_demo/setup.py
@@ -26,7 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'state_publisher = urdf_lunabot.state_publisher:main'
+            'state_publisher = lunabot_demo.state_publisher:main'
         ],
     },
 )
